66d0148759ab4b48dac2a26fb45823338123870d
fribble186 <psj124@qq.com>
2021-08-25 17:37:24 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import JsonFormat from "./components/json-compare"
 
-ReactDOM.render(<div>{JsonFormat({a:1}, {a:2})}</div>, document.getElementById("root"));
+ReactDOM.render(<div>{JsonFormat({a:1, b:2 }, {a:2, c:2})}</div>, document.getElementById("root"));
